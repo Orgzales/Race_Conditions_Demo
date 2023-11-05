7c4f94f628d2bb762e7e1c52fa593e5e136b9474
Orgzales <77345323+Orgzales@users.noreply.github.com>
2023-11-05 17:34:14 -0500

    Ready for Demo & Tested Again + Power Point

--- Race_tool.py
+++ Race_tool.py
@@ -2,9 +2,9 @@ import requests
 import threading
 
 name = 'Orion'
-#url = 'http://52.91.76.219:5000/test'
+url = 'http://52.91.76.219:5000/test' #DONT FORGET TO CHANGE TO YOUR IP AND PORT
 #url = 'http://52.91.76.219:5000/test_fast'
-url = 'http://52.91.76.219:5000/test_fixed'
+#url = 'http://52.91.76.219:5000/test_fixed'
 name_data = {'username': name}
 
 
